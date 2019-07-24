7c1c5469198a3003cf89f97c2f5e326ec4348fca
Eric <eric@canva.com>
2019-07-24 21:15:12 +1000

    chore: refactor to React pattern

--- src/hello-world/tests/hello-world.tests.tsx
+++ src/hello-world/tests/hello-world.tests.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
-import { HelloWorld } from '../hello-world';
+import { Internal } from '../hello-world';
 
 describe('HelloWorld', () => {
+  const { Component } = Internal;
   it('should render correctly without the dynamic chunk', () => {
-    expect(<HelloWorld/>).toMatchRenderedSnapshot();
+    expect(<Component onClick={jest.fn()} BottomSlot={undefined}/>).toMatchRenderedSnapshot();
   });
 
   it('should render correctly with the dynamic chunk', () => {
     const DynamicComponent = () => <div>I'm dynamic</div>;
-    expect(<HelloWorld InitialDynamicComponent={DynamicComponent}/>).toMatchRenderedSnapshot();
+    expect(<Component onClick={jest.fn()} BottomSlot={DynamicComponent}/>).toMatchRenderedSnapshot();
   });
 });
