a849b96c65f7cf93f4664215e5f1efdc953a7b2f
Eric Wang <wjun0912@gmail.com>
2019-08-07 23:40:02 +1000

    Update conf/storybook/config.js

--- conf/storybook/config.js
+++ conf/storybook/config.js
@@ -1,7 +1,6 @@
 import { configure } from '@storybook/react';
 
 function loadStories() {
-  // eslint-disable-next-line global-require
   const req = require.context('../../src', true, /\.stories\.tsx$/);
   req.keys().forEach(filename => req(filename));
 }
